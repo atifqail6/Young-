1a5d9b1a07e9eb32501fd93449dc9c78797c9f7e
atifqail6 <atifqail0@gmail.com>
2025-05-16 20:08:54 +0430

    Update game.js

--- game.js
+++ game.js
@@ -4,18 +4,37 @@ const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let player = { x: 100, y: canvas.height - 150, width: 20, height: 40 };
+// لوبغاړی
+const PLAYER_WIDTH = 40;
+const PLAYER_HEIGHT = 60;
+let player = {
+    x: 100,
+    y: canvas.height - 160,
+    width: PLAYER_WIDTH,
+    height: PLAYER_HEIGHT
+};
+
+// لرګی
+let stick = {
+    length: 0,
+    maxLength: 400,
+    isGrowing: false,
+    dropped: false
+};
+
+// پلیټفارمونه
 let platforms = [];
-let stick = { length: 0, maxLength: 400, isGrowing: false };
+let currentPlatform = 0;
+
+// نمره
 let score = 0;
-let currentPlatformIndex = 0;
 
 // پلیټفارم جوړول
 function createPlatforms() {
     platforms = [
-        { x: 100, width: 100 },
-        { x: 300 + Math.random() * 200, width: 100 },
-        { x: 600 + Math.random() * 200, width: 100 }
+        { x: 100, width: 120 },
+        { x: 350, width: 120 },
+        { x: 600, width: 120 }
     ];
 }
 
@@ -29,7 +48,7 @@ function drawPlayer() {
 
 // پلیټفارمونه رسمول
 function drawPlatforms() {
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "#333";
     platforms.forEach(platform => {
         ctx.fillRect(platform.x, canvas.height - 100, platform.width, 100);
     });
@@ -37,8 +56,8 @@ function drawPlatforms() {
 
 // لرګی رسمول
 function drawStick() {
-    if (stick.isGrowing || stick.length > 0) {
-        ctx.strokeStyle = "black";
+    if (stick.length > 0) {
+        ctx.strokeStyle = "#000";
         ctx.lineWidth = 5;
         ctx.beginPath();
         ctx.moveTo(player.x + player.width, player.y);
@@ -47,14 +66,14 @@ function drawStick() {
     }
 }
 
-// نمرې رسمول
+// نمره رسمول
 function drawScore() {
-    ctx.fillStyle = "black";
+    ctx.fillStyle = "#000";
     ctx.font = "24px Arial";
-    ctx.fillText("Score: " + score, 20, 40);
+    ctx.fillText("Score: " + score, 20, 30);
 }
 
-// لوبه رسمول
+// ګیم رسمول
 function drawGame() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawPlatforms();
@@ -64,16 +83,6 @@ function drawGame() {
     requestAnimationFrame(drawGame);
 }
 
-// کلیک پېښې
-canvas.addEventListener("mousedown", () => {
-    stick.isGrowing = true;
-});
-
-canvas.addEventListener("mouseup", () => {
-    stick.isGrowing = false;
-    dropStick();
-});
-
 // لرګی اوږدول
 function updateStick() {
     if (stick.isGrowing && stick.length < stick.maxLength) {
@@ -81,18 +90,28 @@ function updateStick() {
     }
 }
 
-// لرګی غورځول او کامیاب یا ناکام معلومول
+// د کلیک پېښې
+canvas.addEventListener("mousedown", () => {
+    stick.isGrowing = true;
+});
+
+canvas.addEventListener("mouseup", () => {
+    stick.isGrowing = false;
+    stick.dropped = true;
+    dropStick();
+});
+
 function dropStick() {
-    const targetPlatform = platforms[currentPlatformIndex + 1];
+    const nextPlatform = platforms[currentPlatform + 1];
 
     const stickEndX = player.x + player.width + stick.length;
-    const platformStartX = targetPlatform.x;
-    const platformEndX = targetPlatform.x + targetPlatform.width;
+    const platformStartX = nextPlatform.x;
+    const platformEndX = nextPlatform.x + nextPlatform.width;
 
     if (stickEndX >= platformStartX && stickEndX <= platformEndX) {
         score++;
-        player.x = targetPlatform.x;
-        currentPlatformIndex++;
+        player.x = nextPlatform.x;
+        currentPlatform++;
         addNewPlatform();
     } else {
         alert("Game Over! Final Score: " + score);
@@ -100,20 +119,21 @@ function dropStick() {
     }
 
     stick.length = 0;
+    stick.dropped = false;
 }
 
 // نوی پلیټفارم زیاتول
 function addNewPlatform() {
     const lastPlatform = platforms[platforms.length - 1];
-    const newX = lastPlatform.x + lastPlatform.width + 100 + Math.random() * 150;
-    platforms.push({ x: newX, width: 100 });
+    const newX = lastPlatform.x + lastPlatform.width + 100 + Math.random() * 100;
+    platforms.push({ x: newX, width: 120 });
 }
 
-// لوبه له سره پیلول
+// لوبه له سره پېلول
 function resetGame() {
     player.x = 100;
     score = 0;
-    currentPlatformIndex = 0;
+    currentPlatform = 0;
     createPlatforms();
 }
 
@@ -123,6 +143,6 @@ function gameLoop() {
     requestAnimationFrame(gameLoop);
 }
 
-// لوبه پېل
+// لوبه پیلول
 drawGame();
 gameLoop();
